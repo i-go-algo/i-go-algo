519d2b6abf18a64acf4c79fe31099608a1390b70
YoonSeok Woo <wysf1994@naver.com>
2022-06-28 14:10:42 +0900

    submit: BOJ 01074 Z (yoonseok)

--- BOJ/01074-Z/01074-Z-yoonseok.py
+++ BOJ/01074-Z/01074-Z-yoonseok.py
@@ -1,1 +1,15 @@
 # git commit -m "submit : BOJ 01074 Z (yoonseok)"
+N, r, c = map(int,input().split())
+size = 2**(N-1)
+loc = []
+while (size>=1):
+    loc.append((r//size,c//size))
+    r = r%size
+    c = c%size
+    size=size//2
+size = (2**(N-1))**2
+answer = 0
+for l in loc:
+    answer+=(l[0]*2+l[1])*size
+    size=size//4
+print(answer)
