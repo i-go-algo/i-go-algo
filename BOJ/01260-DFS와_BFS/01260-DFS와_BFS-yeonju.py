cb8f7742371cc5b22fe61740492c980f3510d99c
위연주 <thizzyzz@naver.com>
2022-06-28 19:02:30 +0900

    submit : BOJ 01260 DFS와 BFS (yeonju)

--- BOJ/01260-DFS와_BFS/01260-DFS와_BFS-yeonju.py
+++ BOJ/01260-DFS와_BFS/01260-DFS와_BFS-yeonju.py
@@ -1,1 +1,45 @@
-# git commit -m "submit : BOJ 01260 DFS와 BFS (yeonju)"
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited[start] = 1
+
+    while queue:
+        cur_node = queue.popleft()
+        print(cur_node, end=' ')
+        for next_node in graph[cur_node]:
+            if not visited[next_node]:
+                queue.append(next_node)
+                visited[next_node] = 1
+
+
+def dfs(cur_node):
+    visited[cur_node] = 1
+    print(cur_node, end=' ')
+    for next_node in graph[cur_node]:
+        if not visited[next_node]:
+            dfs(next_node)
+
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n + 1):
+    graph[i].sort()
+
+visited = [0 for _ in range(n + 1)]
+dfs(v)
+
+print()
+visited = [0 for _ in range(n + 1)]
+bfs(v)
