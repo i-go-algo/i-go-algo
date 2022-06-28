009c914345c23f54ba41078b66623515f37af17c
위연주 <thizzyzz@naver.com>
2022-06-28 19:01:16 +0900

    submit : BOJ 01107 리모컨 (yeonju)

--- BOJ/01107-리모컨/01107-리모컨-yeonju.py
+++ BOJ/01107-리모컨/01107-리모컨-yeonju.py
@@ -1,1 +1,17 @@
-# git commit -m "submit : BOJ 01107 리모컨 (yeonju)"
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+li = set(input().split())
+
+ans = abs(100 - n)
+
+for num in range(1000000): 
+    for i in str(num):
+        if i in li:
+            break
+    else:
+        ans = min(ans, len(str(num)) + abs(n - num))
+
+print(ans)
