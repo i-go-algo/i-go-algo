2de7783002c3ac30de8ebdabb84bf784d5e86c19
YoonSeok Woo <wysf1994@naver.com>
2022-06-28 16:04:53 +0900

    submit : BOJ 01107 리모컨 (yoonseok)

--- BOJ/01107-리모컨/01107-리모컨-yoonseok.py
+++ BOJ/01107-리모컨/01107-리모컨-yoonseok.py
@@ -1,6 +1,8 @@
 # git commit -m "submit : BOJ 01107 리모컨 (yoonseok)"
 INF = 10000000000
 def check(num,mf):
+    if num==0 and num in mf:
+        return False
     while num>0:
         temp = num%10
         if temp in mf:
@@ -9,15 +11,17 @@ def check(num,mf):
     return True
 N = int(input())
 nums = int(input())
-mf = []
+
 if nums!=0:
     mf = list(map(int,input().split()))
-buttons = [INF] * N*2
+else:
+    mf = []
+buttons = [INF] *(10000001)
 now = 100
-for i in range(N*2):
+for i in range(max(N*2+1,100)):
     if check(i,mf):
         buttons[i] = abs(N-i)+min(len(str(i)),abs(100-i))
     else:
         buttons[i] = abs(N-100)
+   
 print(min(buttons))
-
