886c89151f91f845e6ef1e64b578c89928de6040
kargo0825 <kargo0825@gmail.com>
2022-06-29 01:03:02 +0900

    submit : BOJ 01260 DFS와 BFS (changjun)

--- BOJ/01107-리모컨/01107-리모컨-changjun.py
+++ BOJ/01107-리모컨/01107-리모컨-changjun.py
@@ -1,1 +1,80 @@
 # git commit -m "submit : BOJ 01107 리모컨 (changjun)"
+
+
+n = list(map(int,input()))
+num = int(''.join(map(str,n)))
+m = int(input())
+res = num+100
+
+if 0<m<10:
+
+    broken = set(map(int,input().split()))
+
+    num_set = set(range(10))
+
+    num_set = num_set - broken
+    min_num = min(num_set)
+    max_num = max(num_set)
+
+    ans = []
+    flag = 0
+
+    for i in n:
+        if not flag:
+            if i in num_set:
+                ans.append(i)
+            else:
+                tmp = 1
+                ans_s = False
+                ans_b = False
+                while i+tmp < 10:
+                    if i+tmp in num_set:
+                        flag = 'small'
+                        ans_s = ans[:]
+                        ans_s.append(i+tmp)
+                        break
+                    tmp += 1 
+                tmp = 1
+                while i-tmp >= 0:
+                    if i-tmp in num_set:
+                        ans_b = ans[:]
+                        ans_b.append(i-tmp)
+                        flag = 'big'
+                        break
+                    tmp += 1 
+
+        else:
+            if ans_b:
+                ans_b.append(max_num)
+            if ans_s:
+                ans_s.append(min_num)
+
+
+    if ans_s:
+        ans_s = int(''.join(map(str,ans_s)))
+        click = len(str(ans_s))
+        res = min(res, click + abs(num-ans_s))
+    if ans_b:
+        ans_b = int(''.join(map(str,ans_b)))
+        click = len(str(ans_b))
+        res = click + abs(ans_b - num)
+
+    if num<200:
+        res = min(res, abs(num - 100))
+    
+    if 1 in num_set:
+        tmp_num = int(''.join(map(str,[1] + [min_num]*click)))
+        res = min(res, tmp_num - num + click + 1)
+
+
+if num<100:
+    res = min(res, 100-num)
+else:
+    res = min(res, num-100)
+if m==10:
+    input()
+if m==0:
+    res = min(res, len(n))
+
+
+print(res)
--- BOJ/01260-DFS와_BFS/01260-DFS와_BFS-changjun.py
+++ BOJ/01260-DFS와_BFS/01260-DFS와_BFS-changjun.py
@@ -1,1 +1,55 @@
 # git commit -m "submit : BOJ 01260 DFS와 BFS (changjun)"
+
+n, m, v = map(int,input().split())
+
+from collections import defaultdict, deque
+
+dic = defaultdict(set)
+
+for _ in range(m):
+    s, e = map(int,input().split())
+
+    dic[s].add(e)
+    dic[e].add(s)
+
+for key, val in dic.items():
+    dic[key] = sorted(val)
+
+dfs = deque()
+bfs = deque()
+
+bfs.append(v)
+dfs.append(v)
+
+bfs_visited = set()
+dfs_visited = set()
+
+bfs_visited.add(v)
+dfs_visited.add(v)
+
+dfs_ans = deque()
+bfs_ans = deque()
+
+dfs_ans.append(v)
+while dfs:
+    tmp = dfs[-1]
+    for node in dic[tmp]:
+        if node not in dfs_visited:
+            dfs_ans.append(node)
+            dfs_visited.add(node)
+            dfs.append(node)
+            break
+    else:
+        dfs.pop()
+
+while bfs:
+    tmp = bfs.popleft()
+    bfs_ans.append(tmp)
+    for node in dic[tmp]:
+        if node not in bfs_visited:
+            bfs_visited.add(node)
+            bfs.append(node)
+
+print(*dfs_ans)
+print(*bfs_ans)
+
