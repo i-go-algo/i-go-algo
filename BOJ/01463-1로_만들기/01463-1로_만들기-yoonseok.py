03544d478ef7d75044058f40f84b033241020b2e
YoonSeok Woo <wysf1994@naver.com>
2022-06-28 19:08:19 +0900

    submit : BOJ 01463 1로 만들기 (yoonseok)

--- BOJ/01463-1로_만들기/01463-1로_만들기-yoonseok.py
+++ BOJ/01463-1로_만들기/01463-1로_만들기-yoonseok.py
@@ -1,1 +1,13 @@
 # git commit -m "submit : BOJ 01463 1로 만들기 (yoonseok)"
+INF = 10000001
+N = int(input())
+count = [INF]*(N+1)
+count[1] = 0
+for i in range(2,N+1):
+    if(i%3==0):
+        count[i]=min(count[i],count[i//3]+1)
+    if(i%2==0):
+        count[i]=min(count[i],count[i//2]+1)
+    count[i] = min(count[i],count[i-1]+1)
+print(count[N])
+    
