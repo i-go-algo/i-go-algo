e58992b31cbcbbdaff1f0af16a422134e679e4c0
YoonSeok Woo <wysf1994@naver.com>
2022-06-30 23:45:25 +0900

    submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (yoonseok)

--- BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yoonseok.py
+++ BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yoonseok.py
@@ -1,1 +1,14 @@
 # git commit -m "submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (yoonseok)"
+N, M = map(int,input().split())
+pocktonum = dict()
+numtopock = ['']*(N+1)
+for i in range(N):
+    temp = input()
+    pocktonum[temp]=i+1
+    numtopock[i+1]=temp
+for j in range(M):
+    question = input()
+    if '0'<=question[0]<='9':
+        print(numtopock[int(question)])
+    else:
+        print(pocktonum[question])
