8a7325eb37bf785497f6c227997603dfd0f669b6
위연주 <thizzyzz@naver.com>
2022-07-01 11:44:31 +0900

    submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (yeonju)

--- BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yeonju.py
+++ BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-yeonju.py
@@ -1,1 +1,11 @@
-# git commit -m "submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (yeonju)"
+n, m = map(int, input().split())
+dic = dict()
+
+for i in range(1, n + 1):
+    pokemon = input()
+    dic[i] = pokemon
+    dic[pokemon] = i
+
+for _ in range(m):
+    check = input()
+    print(dic[int(check)] if check.isdigit() else dic[check])
