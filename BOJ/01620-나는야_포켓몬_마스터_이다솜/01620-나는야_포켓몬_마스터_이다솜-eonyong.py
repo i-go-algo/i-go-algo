7f8bba8e7e412f933ba8309b77342f718e1c225e
정언용 <unjoo94@naver.com>
2022-07-02 17:08:57 +0900

    submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (eonyong)

--- BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-eonyong.py
+++ BOJ/01620-나는야_포켓몬_마스터_이다솜/01620-나는야_포켓몬_마스터_이다솜-eonyong.py
@@ -1,1 +1,16 @@
 # git commit -m "submit : BOJ 01620 나는야 포켓몬 마스터 이다솜 (eonyong)"
+from collections import defaultdict
+
+n, m = map(int, input().split())
+poketToNum, numToPoket = defaultdict(int), defaultdict(str)
+for num in range(1, n + 1):
+    poketmon = input()
+    poketToNum[poketmon] = num
+    numToPoket[num] = poketmon
+
+for _ in range(m):
+    Q = input()
+    if Q.isdigit():
+        print(numToPoket[int(Q)])
+    else:
+        print(poketToNum[Q])
