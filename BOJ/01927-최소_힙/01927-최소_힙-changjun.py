883ed51a8119205715bc61b615c1f1e590cd3219
kargo0825 <kargo0825@gmail.com>
2022-07-05 21:44:15 +0900

    submit : BOJ 01927 최소 힙 (changjun)

--- BOJ/01927-최소_힙/01927-최소_힙-changjun.py
+++ BOJ/01927-최소_힙/01927-최소_힙-changjun.py
@@ -1,1 +1,64 @@
 # git commit -m "submit : BOJ 01927 최소 힙 (changjun)"
+
+from collections import deque
+import sys
+class Heap: 
+
+    def __init__(self):
+        self.val = deque([None])
+        self.lenght = 0
+
+
+ 
+    def insert(self, x):
+        self.lenght += 1
+        l = self.lenght
+        self.val.append(x)
+        while l > 1:
+            if self.val[l] < self.val[l//2]:
+                self.val[l], self.val[l//2] = self.val[l//2] , self.val[l]
+                l = l//2
+            else:
+                break
+
+    def delete(self):
+        if self.lenght == 0:
+            print(0)
+        elif self.lenght == 1:
+            self.lenght -= 1
+            print(self.val.pop())
+        else:            
+            self.lenght -= 1
+            print(self.val[1])
+            self.val[1] = self.val.pop()
+            tmp = 1
+            while 1:
+                if 2*tmp + 1 <= self.lenght:
+                    if self.val[2*tmp] > self.val[2*tmp + 1]:
+                        if self.val[tmp] > self.val[2*tmp + 1]:
+                            self.val[tmp] ,self.val[2*tmp + 1] = self.val[2*tmp + 1] ,self.val[tmp]
+                            tmp = 2*tmp + 1
+                            continue
+                    else:
+                        if self.val[tmp] > self.val[2*tmp]:
+                            self.val[tmp] ,self.val[2*tmp] = self.val[2*tmp] ,self.val[tmp]
+                            tmp = 2*tmp
+                            continue
+
+                elif 2*tmp <= self.lenght:
+                    if self.val[tmp] > self.val[2*tmp]:
+                        self.val[tmp] ,self.val[2*tmp] = self.val[2*tmp] ,self.val[tmp]
+                        tmp = 2*tmp
+                        continue
+                break
+
+
+n = int(input())
+
+heap = Heap()
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        heap.delete()
+    else:
+        heap.insert(x)
