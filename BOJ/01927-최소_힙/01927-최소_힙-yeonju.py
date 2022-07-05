05b05fcd3576ea42690630102c8dd81ea86d50bf
위연주 <thizzyzz@naver.com>
2022-07-05 10:29:21 +0900

    submit : BOJ 01927 최소 힙 (yeonju)

--- BOJ/01927-최소_힙/01927-최소_힙-yeonju.py
+++ BOJ/01927-최소_힙/01927-최소_힙-yeonju.py
@@ -1,1 +1,15 @@
-# git commit -m "submit : BOJ 01927 최소 힙 (yeonju)"
+import sys
+import heapq
+input = sys.stdin.readline
+
+h = []
+n = int(input())
+
+for i in range(n):
+    x = int(input())
+
+    if x:
+        heapq.heappush(h, x)
+    else:
+        print(heapq.heappop(h) if h else 0)
+        
