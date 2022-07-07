092577393251d6c8148f603dcb22b44dadd027ed
위연주 <thizzyzz@naver.com>
2022-07-07 11:59:59 +0900

    submit : BOJ 01780 종이의 개수 (yeonju)

--- BOJ/01780-종이의_개수/01780-종이의_개수-yeonju.py
+++ BOJ/01780-종이의_개수/01780-종이의_개수-yeonju.py
@@ -1,1 +1,32 @@
-# git commit -m "submit : BOJ 01780 종이의 개수 (yeonju)"
+def dfs(x, y, n):
+    global minus_one, zero, one
+    num_check = arr[x][y]
+
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if arr[i][j] != num_check:
+                for k in range(3):
+                    for l in range(3):
+                        dfs(x + k * (n // 3), y + l * (n // 3), n // 3)
+                return
+
+    if num_check == -1:
+        minus_one += 1
+    elif num_check == 0:
+        zero += 1
+    else:
+        one += 1
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+minus_one = 0
+zero = 0
+one = 0
+
+dfs(0, 0, n)
+
+print(minus_one)
+print(zero)
+print(one)
