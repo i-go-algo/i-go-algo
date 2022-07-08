08963f6552ba36a36d8adee0b45b17bbc1919875
위연주 <thizzyzz@naver.com>
2022-07-08 12:43:16 +0900

    submit : BOJ 01931 회의실 배정 (yeonju)

--- BOJ/01931-회의실_배정/01931-회의실_배정-yeonju.py
+++ BOJ/01931-회의실_배정/01931-회의실_배정-yeonju.py
@@ -1,1 +1,17 @@
-# git commit -m "submit : BOJ 01931 회의실 배정 (yeonju)"
+n = int(input())
+li = []
+for i in range(n):
+    start, end = map(int, input().split())
+    li.append((start, end))
+
+ordered = sorted(li, key=lambda x: (x[1], x[0]))
+
+cnt = 0
+end_time = 0
+
+for s, e in ordered:
+    if s >= end_time:
+        cnt += 1
+        end_time = e
+
+print(cnt)
