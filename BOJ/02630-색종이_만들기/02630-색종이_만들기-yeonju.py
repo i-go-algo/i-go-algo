19bd913538d3d1e7fbc8a9456c8ab785b941dfa4
위연주 <thizzyzz@naver.com>
2022-07-11 11:28:59 +0900

    submit : BOJ 02630 색종이 만들기 (yeonju)

--- BOJ/02630-색종이_만들기/02630-색종이_만들기-yeonju.py
+++ BOJ/02630-색종이_만들기/02630-색종이_만들기-yeonju.py
@@ -1,1 +1,28 @@
-# git commit -m "submit : BOJ 02630 색종이 만들기 (yeonju)"
+def dfs(x, y, n):
+    global white, blue
+    color_check = arr[x][y]
+
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if arr[i][j] != color_check:
+                for k in range(2):
+                    for l in range(2):
+                        dfs(x + k * (n // 2), y + l * (n // 2), n // 2)
+                return
+
+    if color_check == 0:
+        white += 1
+    else:
+        blue += 1
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+white = 0
+blue = 0
+
+dfs(0, 0, n)
+
+print(white)
+print(blue)
