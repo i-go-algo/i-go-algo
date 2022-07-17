1e565f011c9e40b231a4be7ab5e1e1f089b4c0a6
위연주 <thizzyzz@naver.com>
2022-07-18 03:51:03 +0900

    submit : BOJ 01043 거짓말 (yeonju)

--- BOJ/01043-거짓말/01043-거짓말-yeonju.py
+++ BOJ/01043-거짓말/01043-거짓말-yeonju.py
@@ -1,1 +1,18 @@
-# git commit -m "submit : BOJ 01043 거짓말 (yeonju)"
+n, m = map(int, input().split())
+truth = set(input().split()[1:])
+cnt = 0
+parties = []
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
+
+# 최악의 경우 역순으로 1번 씩 갱신되기 때문에 m 번 순회해야 함
+for _ in range(m):
+    for party in parties:
+        if party & truth:
+            truth = truth.union(party)
+
+for party in parties:
+    if not party & truth:
+        cnt += 1
+
+print(cnt)
