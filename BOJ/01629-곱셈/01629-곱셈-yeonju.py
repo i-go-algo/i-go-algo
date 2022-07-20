50935a155815ab1002cfaf8e04d876151abf3118
위연주 <thizzyzz@naver.com>
2022-07-20 14:40:23 +0900

    submit : BOJ 01629 곱셈 (yeonju)

--- BOJ/01629-곱셈/01629-곱셈-yeonju.py
+++ BOJ/01629-곱셈/01629-곱셈-yeonju.py
@@ -1,1 +1,13 @@
-# git commit -m "submit : BOJ 01629 곱셈 (yeonju)"
+def power(a, b):
+    if b == 1:
+        return a % c
+
+    temp = power(a, b // 2)
+    if b % 2 == 0:
+        return temp * temp % c
+    else:
+        return temp * temp * a % c
+
+
+a, b, c = map(int, input().split())
+print(power(a, b))
