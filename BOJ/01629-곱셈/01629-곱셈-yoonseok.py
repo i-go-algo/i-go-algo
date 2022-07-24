201c337a05f7bfa802df058a12da9ef748809385
YoonSeok Woo <wysf1994@naver.com>
2022-07-24 18:20:23 +0900

    submit : BOJ 01865 웜홀 (yoonseok)

--- BOJ/01629-곱셈/01629-곱셈-yoonseok.py
+++ BOJ/01629-곱셈/01629-곱셈-yoonseok.py
@@ -1,1 +1,12 @@
 # git commit -m "submit : BOJ 01629 곱셈 (yoonseok)"
+def power(A, B,C):
+    if B==0:
+        return 1
+    temp = power(A,B//2,C)
+    if B%2==0:
+        return temp*temp%C
+    else:
+        return (temp*temp%C)*A%C
+
+A, B, C = map(int,input().split())
+print(power(A,B,C))
