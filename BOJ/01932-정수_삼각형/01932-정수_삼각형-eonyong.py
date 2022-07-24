36d1bb67e717393744e89e77faacfa4aa779d391
정언용 <unjoo94@naver.com>
2022-07-25 01:20:39 +0900

    submit : BOJ 01932 정수 삼각형 (eonyong)

--- BOJ/01932-정수_삼각형/01932-정수_삼각형-eonyong.py
+++ BOJ/01932-정수_삼각형/01932-정수_삼각형-eonyong.py
@@ -1,1 +1,14 @@
 # git commit -m "submit : BOJ 01932 정수 삼각형 (eonyong)"
+answer = 0
+n = int(input())
+boards = [list(map(int, input().split())) for _ in range(n)]
+
+for row in range(1, n):
+    for col in range(row + 1):
+        if not col:
+            boards[row][col] += boards[row - 1][col]
+        elif col == row:
+            boards[row][col] += boards[row - 1][col - 1]
+        else:
+            boards[row][col] += max(boards[row - 1][col], boards[row - 1][col - 1])
+print(max(boards[-1]))
