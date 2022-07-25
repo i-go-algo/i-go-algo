f9a1ecffa06d45d06936387ac48914d57e881fbc
정언용 <unjoo94@naver.com>
2022-07-25 15:43:24 +0900

    submit : BOJ 02096 내려가기 (eonyong)

--- BOJ/02096-내려가기/02096-내려가기-eonyong.py
+++ BOJ/02096-내려가기/02096-내려가기-eonyong.py
@@ -1,1 +1,22 @@
 # git commit -m "submit : BOJ 02096 내려가기 (eonyong)"
+# 창준님 RGB 코드는 신입니다.,...
+
+def minval(boards):
+    r, g, b = boards[0]
+    for x, y, z in boards[1:]:
+        r, g, b = x + min(g, r), y + min(r, g, b), z + min(b, g)
+    return min(r, g, b)
+
+
+def maxval(boards):
+    r, g, b = boards[0]
+    for x, y, z in boards[1:]:
+        r, g, b = x + max(g, r), y + max(r, g, b), z + max(b, g)
+    return max(r, g, b)
+
+
+n = int(input())
+
+boards = [list(map(int, input().split())) for _ in range(n)]
+
+print(maxval(boards), minval(boards))
