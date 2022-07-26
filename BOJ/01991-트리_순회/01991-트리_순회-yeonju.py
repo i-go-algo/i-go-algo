c65599b7618411a3862306346705222ee3f6b9fa
위연주 <thizzyzz@naver.com>
2022-07-26 17:36:14 +0900

    submit : BOJ 01991 트리 순회 (yeonju)

--- BOJ/01991-트리_순회/01991-트리_순회-yeonju.py
+++ BOJ/01991-트리_순회/01991-트리_순회-yeonju.py
@@ -1,1 +1,40 @@
-# git commit -m "submit : BOJ 01991 트리 순회 (yeonju)"
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def preorder(node):
+    if node == '.':
+        return
+    print(node, end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+
+
+def inorder(node):  # 중위 순회
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node, end='')
+    inorder(tree[node][1])
+
+
+def postorder(node):
+    if node == '.':
+        return
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node, end='')
+
+
+n = int(input())
+tree = {}
+for _ in range(n):
+    root, left, right = input().split()
+    tree[root] = (left, right)
+
+print(tree)
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
