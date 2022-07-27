59e8f81e197c33d5c18fda0134e8dfba320a80b4
kargo0825 <kargo0825@gmail.com>
2022-07-27 21:45:38 +0900

    submit : BOJ 01916 최소비용 구하기 (changjun)

--- BOJ/01916-최소비용_구하기/01916-최소비용_구하기-changjun.py
+++ BOJ/01916-최소비용_구하기/01916-최소비용_구하기-changjun.py
@@ -1,1 +1,37 @@
 # git commit -m "submit : BOJ 01916 최소비용 구하기 (changjun)"
+
+from collections import defaultdict
+import heapq
+
+n = int(input())
+m = int(input())
+
+dic = defaultdict(set)
+for _ in range(m):
+    s, e, w = map(int,input().split())
+    dic[s].add((e,w))
+
+s, e = map(int,input().split())
+
+big = 100000*m*n
+lst = [big]*(n+1)
+lst[s] = 0
+q = []
+heapq.heappush(q, (s,0))
+
+while q:
+    node, cost = heapq.heappop(q)
+    if lst[node] < cost:
+        continue
+    for new_node, cost in dic[node]:
+        if lst[new_node] > lst[node] + cost:
+            lst[new_node] = lst[node] + cost
+            heapq.heappush(q, (new_node, lst[new_node]))
+print(lst[e])
+
+
+
+
+
+
+
