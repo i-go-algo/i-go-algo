94157a7cad8330e9434a5eac536b0379ead7cfb5
위연주 <thizzyzz@naver.com>
2022-07-30 15:52:51 +0900

    submit : BOJ 02407 조합 (yeonju)

--- BOJ/02407-조합/02407-조합-yeonju.py
+++ BOJ/02407-조합/02407-조합-yeonju.py
@@ -1,1 +1,14 @@
-# git commit -m "submit : BOJ 02407 조합 (yeonju)"
+import sys
+input= sys.stdin.readline
+
+n, m = map(int, input().split())
+
+numerator = 1
+denominator = 1
+
+for i in range(n, n - m, -1):
+    numerator *= i
+for i in range(1, m + 1):
+    denominator *= i
+
+print(numerator // denominator)
