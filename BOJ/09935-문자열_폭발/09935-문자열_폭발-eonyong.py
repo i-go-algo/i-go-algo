f3c4a33e4200d1338507e6ad63df1dab79ee7744
정언용 <unjoo94@naver.com>
2022-08-07 16:58:44 +0900

    submit : BOJ 09935 문자열 폭발 (eonyong)

--- BOJ/09935-문자열_폭발/09935-문자열_폭발-eonyong.py
+++ BOJ/09935-문자열_폭발/09935-문자열_폭발-eonyong.py
@@ -1,1 +1,13 @@
 # git commit -m "submit : BOJ 09935 문자열 폭발 (eonyong)"
+mails = input()
+changes = list(input())
+c = len(changes)
+queue = []
+for mail in mails:
+    queue.append(mail)
+    if len(queue) >= c and queue[-c:] == changes:
+        for _ in range(c):
+            queue.pop()
+else:
+    queue = ''.join(queue)
+    print(queue) if queue else print('FRULA')
