10662d192216187919629d74c5f5a2c6666370e6
위연주 <thizzyzz@naver.com>
2022-08-08 17:57:18 +0900

    submit : BOJ 09935 문자열 폭발 (yeonju)

--- BOJ/09935-문자열_폭발/09935-문자열_폭발-yeonju.py
+++ BOJ/09935-문자열_폭발/09935-문자열_폭발-yeonju.py
@@ -1,1 +1,16 @@
-# git commit -m "submit : BOJ 09935 문자열 폭발 (yeonju)"
+words = input()
+explosive_word = list(input())
+st = []
+
+for word in words:
+    st.append(word)
+
+    if word == explosive_word[-1]:
+        if st[len(st) - len(explosive_word): len(st)] == explosive_word:
+            for _ in range(len(explosive_word)):
+                st.pop()
+
+if st:
+    print(''.join(st))
+else:
+    print('FRULA')
