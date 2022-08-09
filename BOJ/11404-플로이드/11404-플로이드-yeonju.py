f42a61ce588e653f091f9bd1b02a5d85048305d1
위연주 <thizzyzz@naver.com>
2022-08-09 10:37:40 +0900

    submit : BOJ 11404 플로이드 (yeonju)

--- BOJ/11404-플로이드/11404-플로이드-yeonju.py
+++ BOJ/11404-플로이드/11404-플로이드-yeonju.py
@@ -1,1 +1,33 @@
-# git commit -m "submit : BOJ 11404 플로이드 (yeonju)"
+import sys
+input = sys.stdin.readline
+
+
+def floyd_warshall():
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if i != j and arr[i][j] > arr[i][k] + arr[k][j]:
+                    arr[i][j] = arr[i][k] + arr[k][j]
+
+    return arr
+
+
+n = int(input())
+m = int(input())
+INF = sys.maxsize
+arr = [[INF for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if c < arr[a - 1][b - 1]:
+        arr[a - 1][b - 1] = c
+
+floyd_warshall()
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(arr[i][j], end=' ')
+    print()
