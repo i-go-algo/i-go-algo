2765b9647d8a520fd091a651c3f647a75d60845a
정언용 <unjoo94@naver.com>
2022-08-09 20:16:03 +0900

    fix : BOJ 11404 플로이드 (eonyong)

--- BOJ/11404-플로이드/11404-플로이드-eonyong.py
+++ BOJ/11404-플로이드/11404-플로이드-eonyong.py
@@ -6,14 +6,15 @@ for _ in range(m):
     s, e, w = map(int, input().split())
     dist[s - 1][e - 1] = min(dist[s - 1][e - 1], w)
 
-for idx in range(n):
-    dist[idx][idx] = 0
 
 for start in range(n):
     for middle in range(n):
         for end in range(n):
             dist[middle][end] = min(dist[middle][end], dist[middle][start] + dist[start][end])
-            
-for ans in dist:
-    print(*ans)
-    
+
+for row in range(n):
+    for col in range(n):
+        if dist[row][col] == float('inf') or row == col:
+            dist[row][col] = 0
+    else:
+        print(*dist[row])
