519f31e869b339a5d4dc039cdda77a49da5ec30d
YoonSeok Woo <wysf1994@naver.com>
2022-08-09 15:39:43 +0900

    submit : BOJ 11404 플로이드 (yoonseok)

--- BOJ/11404-플로이드/11404-플로이드-yoonseok.py
+++ BOJ/11404-플로이드/11404-플로이드-yoonseok.py
@@ -1,1 +1,28 @@
 # git commit -m "submit : BOJ 11404 플로이드 (yoonseok)"
+n = int(input())
+m = int(input())
+INF = 987654321
+cost = [[INF]*n for i in range(n)]
+for i in range(n):
+    cost[i][i] = 0
+for i in range(m):
+    i,j,c = map(int,input().split())
+    cost[i-1][j-1] = min(cost[i-1][j-1],c)
+
+for i in range(n):
+    print(*cost[i])
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            
+            if (cost[i][j]>cost[i][k]+cost[k][j]):
+                
+                cost[i][j] = cost[i][k]+cost[k][j]
+for i in range(n):
+    for j in range(n):
+        if cost[i][j] == INF:
+            print(0, end = ' ')
+        else:
+            print(cost[i][j],end = ' ')
+    print()
