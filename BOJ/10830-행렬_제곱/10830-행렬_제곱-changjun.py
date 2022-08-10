a8fe0276eba90a84f8a8b8b19f1abe337085fe92
kargo0825 <kargo0825@gmail.com>
2022-08-10 22:51:45 +0900

    submit : BOJ 10830 행렬 제곱 (changjun)

--- BOJ/10830-행렬_제곱/10830-행렬_제곱-changjun.py
+++ BOJ/10830-행렬_제곱/10830-행렬_제곱-changjun.py
@@ -1,1 +1,36 @@
 # git commit -m "submit : BOJ 10830 행렬 제곱 (changjun)"
+
+n, x = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+
+def matmul(lst1, lst2):
+    ans = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            tmp = 0
+            for k in range(n):
+                tmp += lst1[i][k] * lst2[k][j]
+            ans[i][j] = tmp % 1000
+    return ans
+
+
+def func(input_lst, x):
+    if x == 1:
+        for i in range(n):
+            for j in range(n):
+                input_lst[i][j] = input_lst[i][j] % 1000
+        return input_lst
+
+    elif x % 2 == 0:
+        tmp = func(input_lst, x // 2)
+        return matmul(tmp, tmp)
+    else:
+        tmp = func(input_lst, x // 2)
+        return matmul(matmul(tmp, tmp), lst)
+
+
+ans = func(lst, x)
+
+for line in ans:
+    print(*line)
