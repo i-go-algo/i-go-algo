18e07d63299f400a37011abd6a89e2275838374d
kargo0825 <kargo0825@gmail.com>
2022-08-15 18:51:48 +0900

    submit : BOJ 11725 트리의 부모 찾기 (changjun)

--- BOJ/11725-트리의_부모_찾기/11725-트리의_부모_찾기-changjun.py
+++ BOJ/11725-트리의_부모_찾기/11725-트리의_부모_찾기-changjun.py
@@ -1,1 +1,25 @@
 # git commit -m "submit : BOJ 11725 트리의 부모 찾기 (changjun)"
+from collections import defaultdict, deque
+n = int(input())
+dic = defaultdict(set)
+for _ in range(n-1):
+    s, e = map(int,input().split())
+    dic[s].add(e)
+    dic[e].add(s)
+
+lst = [0]*(n+1)
+
+q = deque()
+q.append(1)
+lst[1] = 1
+
+while q:
+    x = q.popleft()
+    for node in dic[x]:
+        if not lst[node]:
+            lst[node] = x
+            q.append(node)
+
+for i in range(2, n+1):
+    print(lst[i])
+
