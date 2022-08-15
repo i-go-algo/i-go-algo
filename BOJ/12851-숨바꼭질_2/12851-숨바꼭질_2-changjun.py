6fa923498d5eb20a95bed281325c1d25aa50c42b
kargo0825 <kargo0825@gmail.com>
2022-08-15 19:08:03 +0900

    submit : BOJ 12851 숨바꼭질 2 (changjun)

--- BOJ/12851-숨바꼭질_2/12851-숨바꼭질_2-changjun.py
+++ BOJ/12851-숨바꼭질_2/12851-숨바꼭질_2-changjun.py
@@ -1,1 +1,37 @@
 # git commit -m "submit : BOJ 12851 숨바꼭질 2 (changjun)"
+
+from collections import deque
+
+n, k = map(int,input().split())
+max_num = int(max(n,k)*1.5)
+q = deque()
+q.append((n, 0))
+
+match = 0
+cnt = 0
+
+lst = [100000]*(max_num)
+
+while q:
+    position, time = q.popleft()
+    for next in [position-1, position+1, 2*position]:
+        if next == k:
+            match = 1
+            cnt += 1
+            arrive_time = time
+        if 0<=next<max_num and lst[next] > time:
+            lst[next] = time + 1
+            q.append((next, time+1))
+
+    if match:
+        if time != arrive_time:
+            break
+print(time)
+print(cnt)
+
+
+    
+
+
+
+
