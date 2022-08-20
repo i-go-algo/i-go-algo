0bb4c903653caca652899da93c02edf89d39ce2f
정언용 <unjoo94@naver.com>
2022-08-20 23:27:58 +0900

    submit : BOJ 15650 N과 M (2) (eonyong)

--- BOJ/15650-N과_M_(2)/15650-N과_M_(2)-eonyong.py
+++ BOJ/15650-N과_M_(2)/15650-N과_M_(2)-eonyong.py
@@ -1,1 +1,12 @@
 # git commit -m "submit : BOJ 15650 N과 M (2) (eonyong)"
+import sys
+from itertools import combinations
+
+n, m = map(int, sys.stdin.readline().split())
+
+combi = list(combinations(range(1, n + 1), m))
+
+for num in combi:
+    for i in num:
+        print(i, end=' ')
+    print('')
