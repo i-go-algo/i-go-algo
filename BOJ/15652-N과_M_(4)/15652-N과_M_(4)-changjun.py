b7656b948f93c160b7beb4006a2a2025198afa50
kargo0825 <kargo0825@gmail.com>
2022-08-22 00:25:01 +0900

    submit : BOJ 15652 N과 M (4) (changjun)

--- BOJ/15652-N과_M_(4)/15652-N과_M_(4)-changjun.py
+++ BOJ/15652-N과_M_(4)/15652-N과_M_(4)-changjun.py
@@ -1,1 +1,25 @@
 # git commit -m "submit : BOJ 15652 N과 M (4) (changjun)"
+
+n, m = map(int,input().split())
+
+lst = range(1, n+1)
+
+
+def func(ans):
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in lst:
+        if ans:
+            if ans[-1] <= i:
+                ans.append(i)
+                func(ans)
+                ans.pop()
+        else:
+            ans.append(i)
+            func(ans)
+            ans.pop()
+
+        
+
+func([])
