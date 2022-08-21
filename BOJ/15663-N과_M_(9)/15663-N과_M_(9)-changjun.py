1c7168049a7e9ad34f0acd15a49c1637989d2e07
kargo0825 <kargo0825@gmail.com>
2022-08-22 00:36:13 +0900

    submit : BOJ 15663 N과 M (9) (changjun)

--- BOJ/15663-N과_M_(9)/15663-N과_M_(9)-changjun.py
+++ BOJ/15663-N과_M_(9)/15663-N과_M_(9)-changjun.py
@@ -1,1 +1,12 @@
 # git commit -m "submit : BOJ 15663 N과 M (9) (changjun)"
+
+from itertools import permutations
+
+n, m = map(int,input().split())
+
+lst = list(map(int,input().split()))
+
+combi = sorted(list(set(permutations(lst, m))))
+
+for nums in combi:
+    print(*nums)
