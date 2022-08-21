eff2238ad0fae8ef69d2153f04369e9d534b3878
kargo0825 <kargo0825@gmail.com>
2022-08-22 00:24:49 +0900

    submit : BOJ 15650 N과 M (2) (changjun)

--- BOJ/15650-N과_M_(2)/15650-N과_M_(2)-changjun.py
+++ BOJ/15650-N과_M_(2)/15650-N과_M_(2)-changjun.py
@@ -1,1 +1,8 @@
 # git commit -m "submit : BOJ 15650 N과 M (2) (changjun)"
+
+from itertools import combinations
+
+n, m = map(int,input().split())
+
+for nums in combinations(range(1,n+1),m):
+    print(*nums)
