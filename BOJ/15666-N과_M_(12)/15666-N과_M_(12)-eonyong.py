c8ff458a112c4440a1a1ec073daba82ea2df90e3
정언용 <unjoo94@naver.com>
2022-08-24 16:27:00 +0900

    submit : BOJ 15666 N과 M (12) (eonyong)

--- BOJ/15666-N과_M_(12)/15666-N과_M_(12)-eonyong.py
+++ BOJ/15666-N과_M_(12)/15666-N과_M_(12)-eonyong.py
@@ -1,1 +1,8 @@
 # git commit -m "submit : BOJ 15666 N과 M (12) (eonyong)"
+import sys
+from itertools import combinations_with_replacement
+
+n, m = map(int, sys.stdin.readline().split())
+arr = sorted(map(int, sys.stdin.readline().split()))
+for cwp in sorted(set(combinations_with_replacement(arr, m))):
+    print(*cwp)
