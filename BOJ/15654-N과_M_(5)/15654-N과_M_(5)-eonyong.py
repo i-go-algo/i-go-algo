37f18bf503cf2910aa59d89e102555acbbc8f775
정언용 <unjoo94@naver.com>
2022-08-24 16:22:14 +0900

    submit : BOJ 15654 N과 M (5) (eonyong)

--- BOJ/15654-N과_M_(5)/15654-N과_M_(5)-eonyong.py
+++ BOJ/15654-N과_M_(5)/15654-N과_M_(5)-eonyong.py
@@ -1,1 +1,19 @@
 # git commit -m "submit : BOJ 15654 N과 M (5) (eonyong)"
+import sys
+from itertools import permutations
+
+
+def Prunes(i, n, arr, ans):
+    if i == n:
+        print(ans)
+    else:
+        for j in range(n):
+            ans.append(arr[j])
+            Prunes(i + 1, n, arr, ans)
+            ans.pop()
+
+
+n, m = map(int, sys.stdin.readline().split())
+arr = sorted(list(map(int, sys.stdin.readline().split())))
+for permu in sorted(set(permutations(arr, m))):
+    print(*permu)
