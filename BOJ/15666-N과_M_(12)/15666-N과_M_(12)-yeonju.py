63ff536e0626ea5f2c203f379f07f72f4993e505
위연주 <thizzyzz@naver.com>
2022-08-24 23:54:49 +0900

    submit : BOJ 15663 N과 M (12) (yeonju)

--- BOJ/15666-N과_M_(12)/15666-N과_M_(12)-yeonju.py
+++ BOJ/15666-N과_M_(12)/15666-N과_M_(12)-yeonju.py
@@ -1,1 +1,19 @@
+def dfs(start):
+    if len(temp) == m:
+        print(' '.join(map(str, temp)))
+        return
+
+    for i in range(start, len(nums)):
+        temp.append(nums[i])
+        dfs(i)
+        temp.pop()
+
+
+n, m = map(int, input().split())
+nums = sorted(set(list(map(int, input().split()))))
+temp = []
+
+dfs(0)
+
+
 # git commit -m "submit : BOJ 15666 N과 M (12) (yeonju)"
