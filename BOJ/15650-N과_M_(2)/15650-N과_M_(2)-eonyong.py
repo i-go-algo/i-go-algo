b539df689a05b7b795819a65524eae4b5669783c
정언용 <unjoo94@naver.com>
2022-08-25 23:46:25 +0900

    submit : BOJ 15650 N과 M (2) (eonyong)

--- BOJ/15650-N과_M_(2)/15650-N과_M_(2)-eonyong.py
+++ BOJ/15650-N과_M_(2)/15650-N과_M_(2)-eonyong.py
@@ -4,9 +4,5 @@ from itertools import combinations
 
 n, m = map(int, sys.stdin.readline().split())
 
-combi = list(combinations(range(1, n + 1), m))
-
-for num in combi:
-    for i in num:
-        print(i, end=' ')
-    print('')
+for num in combinations(range(1, n + 1), m):
+    print(*num)
