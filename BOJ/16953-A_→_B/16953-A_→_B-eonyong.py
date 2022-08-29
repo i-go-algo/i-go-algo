4770f40170ea62ff654a79922358e87dfce43f25
정언용 <unjoo94@naver.com>
2022-08-29 22:40:06 +0900

    submit : BOJ 16953 A → B (eonyong)

--- BOJ/16953-A_→_B/16953-A_→_B-eonyong.py
+++ BOJ/16953-A_→_B/16953-A_→_B-eonyong.py
@@ -1,1 +1,17 @@
 # git commit -m "submit : BOJ 16953 A → B (eonyong)"
+import sys
+
+
+def GoToEnd(s, e, cnt):
+    global answer
+    if s == e:
+        answer = min(answer, cnt)
+    elif s < end:
+        for m in [s * 2, 10 * s + 1]:
+            GoToEnd(m, e, cnt + 1)
+
+
+answer = float('inf')
+start, end = map(int, sys.stdin.readline().split())
+GoToEnd(start, end, 1)
+print(answer) if answer != float('inf') else print(-1)
