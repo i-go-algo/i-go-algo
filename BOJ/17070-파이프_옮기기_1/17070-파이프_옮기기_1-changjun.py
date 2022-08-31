2bf2c51a6d799c13aba0c509eb5b1cbe02b9e9ec
kargo0825 <kargo0825@gmail.com>
2022-09-01 00:25:53 +0900

    submit : BOJ 17070 파이프 옮기기 1 (changjun)

--- BOJ/17070-파이프_옮기기_1/17070-파이프_옮기기_1-changjun.py
+++ BOJ/17070-파이프_옮기기_1/17070-파이프_옮기기_1-changjun.py
@@ -1,1 +1,23 @@
 # git commit -m "submit : BOJ 17070 파이프 옮기기 1 (changjun)"
+
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+cnt = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
+cnt[0][1][0] = 1
+
+for i in range(n):
+    for j in range(1, n):
+
+        if j + 1 < n and grid[i][j + 1] == 0:
+            cnt[i][j + 1][0] = cnt[i][j + 1][0] + cnt[i][j][0]
+            cnt[i][j + 1][0] = cnt[i][j + 1][0] + cnt[i][j][2]
+        if i + 1 < n and grid[i + 1][j] == 0:
+            cnt[i + 1][j][1] = cnt[i + 1][j][1] + cnt[i][j][1]
+            cnt[i + 1][j][1] = cnt[i + 1][j][1] + cnt[i][j][2]
+        if i + 1 < n and j + 1 < n and grid[i + 1][j + 1] == 0 and grid[i + 1][j] == 0 and grid[i][j + 1] == 0:
+            cnt[i + 1][j + 1][2] = cnt[i + 1][j + 1][2] + cnt[i][j][0]
+            cnt[i + 1][j + 1][2] = cnt[i + 1][j + 1][2] + cnt[i][j][1]
+            cnt[i + 1][j + 1][2] = cnt[i + 1][j + 1][2] + cnt[i][j][2]
+answer = sum(cnt[-1][-1])
+print(answer)
