a1a0a00b09e5e688802f53f8d58fc6703986fa5f
YoonSeok Woo <wysf1994@naver.com>
2022-09-07 14:50:23 +0900

    submit : BOJ 13458 시험 감독 (yoonseok)

--- BOJ/13458-시험_감독/13458-시험_감독-yoonseok.py
+++ BOJ/13458-시험_감독/13458-시험_감독-yoonseok.py
@@ -1,1 +1,15 @@
 # git commit -m "submit : BOJ 13458 시험 감독 (yoonseok)"
+N = int(input())
+A = list(map(int,input().split()))
+B, C = map(int,input().split())
+ans = 0
+for i in range(N):
+    ans += 1
+    remain = A[i]-B
+    if remain<=0:
+        continue
+    if remain%C:
+        ans+=remain//C+1
+    else:
+        ans+=remain//C
+print(ans)
