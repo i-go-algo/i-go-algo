8d39fbdc9a0483fff827dc81240dc6d40bf497c6
위연주 <thizzyzz@naver.com>
2022-09-15 08:13:13 +0900

    submit : BOJ 14503 로봇 청소기 (yeonju)

--- BOJ/14503-로봇_청소기/14503-로봇_청소기-yeonju.py
+++ BOJ/14503-로봇_청소기/14503-로봇_청소기-yeonju.py
@@ -1,1 +1,42 @@
-# git commit -m "submit : BOJ 14503 로봇 청소기 (yeonju)"
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n,m = map(int,input().split())
+graph = []
+visited = [[0] * m for _ in range(n)]
+r,c,d = map(int,input().split())
+
+# d - 0,3,2,1 순서
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+visited[r][c] = 1
+cnt = 1
+
+while 1:
+    flag = 0
+    for _ in range(4):
+        
+        nx = r + dx[(d+3)%4]
+        ny = c + dy[(d+3)%4]
+        
+        d = (d+3)%4
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                cnt += 1
+                r = nx
+                c = ny
+                
+                flag = 1
+                break
+    if flag == 0: 
+        if graph[r-dx[d]][c-dy[d]] == 1:
+            print(cnt)
+            break
+        else:
+            r,c = r-dx[d],c-dy[d]
