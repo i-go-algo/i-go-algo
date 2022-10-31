5b0d9f260d70412eded9d4f7f99cafcc3e2dc6cf
정언용 <unjoo94@naver.com>
2022-10-31 14:07:08 +0900

    submit : BOJ 01197 최소 스패닝 트리 (eonyong)

--- BOJ/01197-최소_스패닝_트리/01197-최소_스패닝_트리-eonyong.py
+++ BOJ/01197-최소_스패닝_트리/01197-최소_스패닝_트리-eonyong.py
@@ -1,1 +1,40 @@
 # git commit -m "submit : BOJ 01197 최소 스패닝 트리 (eonyong)"
+import sys
+
+input = sys.stdin.readline
+
+
+def Union(s, e, parents):
+    s = Find(s)
+    e = Find(e)
+    if e < s:
+        parents[s] = e
+    else:
+        parents[e] = s
+    return parents
+
+
+def Find(v):
+    if v == parents[v]:
+        return v
+    parents[v] = Find(parents[v])
+    return parents[v]
+
+
+answer = 0
+n, m = map(int, input().split())
+nodes = []
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    nodes.append([s, e, w])
+
+nodes.sort(key=lambda x: x[2])
+
+parents = list(range(n + 1))
+
+for s, e, w in nodes:
+    if Find(s) != Find(e):
+        parents = Union(s, e, parents)
+        answer += w
+
+print(answer)
