8cd7a3fa0bdd795cdb2ab0439e5b3bc706d6d048
위연주 <thizzyzz@naver.com>
2022-11-03 14:02:23 +0900

    submit : BOJ 01208 부분수열의 합 2 (yeonju)

--- BOJ/01208-부분수열의_합_2/01208-부분수열의_합_2-yeonju.py
+++ BOJ/01208-부분수열의_합_2/01208-부분수열의_합_2-yeonju.py
@@ -1,1 +1,27 @@
-# git commit -m "submit : BOJ 01208 부분수열의 합 2 (yeonju)"
+from itertools import combinations
+from collections import defaultdict
+
+
+def get_subsequence(li, res):
+    for count in range(1, len(nums) + 1):
+        for combi in combinations(li, count):
+            total_value = sum(combi)
+            res[total_value] += 1
+
+
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+
+sub_sum1 = defaultdict(int)
+sub_sum2 = defaultdict(int)
+
+get_subsequence(nums[n // 2:], sub_sum1)
+get_subsequence(nums[:n // 2], sub_sum2)
+
+answer = sub_sum1[s] + sub_sum2[s]
+
+for s1 in sub_sum1:
+    if s - s1 in sub_sum2:
+        answer += sub_sum1[s1] * sub_sum2[s - s1]
+        
+print(answer)
