b94b1ad3f1582a1eb3eba7d0ec438fdeee00b8e0
위연주 <thizzyzz@naver.com>
2022-11-25 20:17:10 +0900

    submit : BOJ 01644 소수의 연속합 (yeonju)

--- BOJ/01644-소수의_연속합/01644-소수의_연속합-yeonju.py
+++ BOJ/01644-소수의_연속합/01644-소수의_연속합-yeonju.py
@@ -1,1 +1,31 @@
-# git commit -m "submit : BOJ 01644 소수의 연속합 (yeonju)"
+n = int(input())
+
+prime_or_not = [False, False] + [True for i in range(n - 1)]
+arr = []
+
+for i in range(2, int(n ** 0.5) + 1):
+    if prime_or_not[i]:
+        prime_or_not[i * 2 :: i] = [False] * ((n - i) // i)
+
+for i in range(n + 1):
+    if prime_or_not[i] is True:
+        arr.append(i)
+
+i, j = 0, 0
+total = 0
+cnt = 0
+
+while True:
+    if total == n:
+        cnt += 1
+
+    if total > n:
+        total -= arr[i]
+        i += 1
+    elif j == len(arr):
+        break
+    else:
+        total += arr[j]
+        j += 1
+
+print(cnt)
