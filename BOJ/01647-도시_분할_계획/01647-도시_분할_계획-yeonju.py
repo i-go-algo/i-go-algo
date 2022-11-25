426ddbf5a4d5cccaba7a1e9b2202ff604a5bc1ee
위연주 <thizzyzz@naver.com>
2022-11-25 20:18:04 +0900

    submit : BOJ 01647 도시 분할 계획 (yeonju)

--- BOJ/01647-도시_분할_계획/01647-도시_분할_계획-yeonju.py
+++ BOJ/01647-도시_분할_계획/01647-도시_분할_계획-yeonju.py
@@ -1,1 +1,34 @@
-# git commit -m "submit : BOJ 01647 도시 분할 계획 (yeonju)"
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph.append((c, a, b))
+
+parent = [i for i in range(n + 1)]
+
+graph.sort()
+res = []
+
+for c, a, b in graph:
+    if find_parent(a) != find_parent(b):
+        union_parent(a, b)
+        res.append(c)
+
+print(sum(res[:-1]))
