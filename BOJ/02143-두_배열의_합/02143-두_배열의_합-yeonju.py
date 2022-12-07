e680b68431bc58ed64f9b0b68d74b90466cf4b70
위연주 <thizzyzz@naver.com>
2022-12-07 18:42:02 +0900

    submit : BOJ 02143 두 배열의 합 (yeonju)

--- BOJ/02143-두_배열의_합/02143-두_배열의_합-yeonju.py
+++ BOJ/02143-두_배열의_합/02143-두_배열의_합-yeonju.py
@@ -1,1 +1,20 @@
-# git commit -m "submit : BOJ 02143 두 배열의 합 (yeonju)"
+from collections import defaultdict
+
+t = int(input())
+n = int(input())
+a_nums = list(map(int, input().split()))
+m = int(input())
+b_nums = list(map(int, input().split()))
+
+ans = 0
+cnt_dict = defaultdict(int)
+
+for i in range(n):
+    for j in range(i, n):
+        cnt_dict[sum(a_nums[i:j + 1])] += 1
+
+for i in range(m):
+    for j in range(i, m):
+        ans += cnt_dict[t - sum(b_nums[i:j + 1])]
+
+print(ans)
