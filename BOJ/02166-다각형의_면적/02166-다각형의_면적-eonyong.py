53edcaa7bced29f06c858366e27ab41b5ded7eaa
정언용 <unjoo94@naver.com>
2022-12-12 13:46:31 +0900

    submit : BOJ 02166 다각형의 면적 (eonyong)

--- BOJ/02166-다각형의_면적/02166-다각형의_면적-eonyong.py
+++ BOJ/02166-다각형의_면적/02166-다각형의_면적-eonyong.py
@@ -1,1 +1,13 @@
 # git commit -m "submit : BOJ 02166 다각형의 면적 (eonyong)"
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+pos = [list(map(int, input().split())) for _ in range(n)]
+
+a = 0
+for idx in range(n):
+    a += pos[idx][0] * pos[(idx + 1) % n][1] - pos[idx][1] * pos[(idx + 1) % n][0]
+print(round(abs(a) / 2, 1))
