57ac7648d0249da55a73917c53f2645c0f95e89b
YoonSeok Woo <wysf1994@naver.com>
2022-12-14 20:04:03 +0900

    submit : BOJ 02166 다각형의 면적 (yoonseok)

--- BOJ/02166-다각형의_면적/02166-다각형의_면적-yoonseok.py
+++ BOJ/02166-다각형의_면적/02166-다각형의_면적-yoonseok.py
@@ -1,1 +1,13 @@
 # git commit -m "submit : BOJ 02166 다각형의 면적 (yoonseok)"
+n = int(input())
+x, y = [], []
+answer = 0
+for _ in range(n):
+    a, b = map(int,input().split())
+    x.append(a)
+    y.append(b)
+x, y = x + [x[0]], y + [y[0]]
+
+for i in range(n):
+    answer += (x[i]*y[i+1])-(x[i+1]*y[i])
+print(round(abs(answer)/2,1))
